0bda202c43aa25e74dcebd8b6ceb0dad6385e44b
Contra <nano2012rus@yandex.ru>
2023-09-17 17:27:49 +0400

    .net standart 2.0

--- Application/Program.cs
+++ Application/Program.cs
@@ -6,74 +6,81 @@ using SteamLibrary;
 using SteamLibrary.JSONs;
 using SteamLibrary.Services;
 
-namespace Application;
-
-public class Program
+namespace Application
 {
-    public static void Main(string[] args){
-        List<Client> clients = new List<Client>();
-        foreach (Credentials cred in JSONUtils.ParseUsers(args[0])) {
-            clients.Add(new Client(cred));
-        }
-        foreach (Client client in clients)
+
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            new Thread(client.Start).Start();
+            List<Client> clients = new List<Client>();
+            foreach (Credentials cred in JSONUtils.ParseUsers(args[0]))
+            {
+                clients.Add(new Client(cred));
+            }
+            foreach (Client client in clients)
+            {
+                new Thread(client.Start).Start();
+            }
+            //new Thread(clients[0].Start).Start();
+            //var run = true;
+            // Thread th = new Thread(() =>
+            // {
+            //     while (run)
+            //     {
+            //         run = Console.ReadKey(false).KeyChar != 'p';
+            //         Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            //     }
+            // });
+            // th.Start();
+            //while (run) {
+            //    Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            //}
+            //Console.WriteLine();
+            //foreach (Client client in clients)
+            //{
+            //    client.Stop();
+            //}
         }
-        //new Thread(clients[0].Start).Start();
-        var run = true;
-        // Thread th = new Thread(() =>
-        // {
-        //     while (run)
-        //     {
-        //         run = Console.ReadKey(false).KeyChar != 'p';
-        //         Thread.Sleep(TimeSpan.FromMilliseconds(100));
-        //     }
-        // });
-        // th.Start();
-        while (run) {
-            Thread.Sleep(TimeSpan.FromMilliseconds(100));
-        }
-        Console.WriteLine();
-        foreach (Client client in clients)
+
+        class Client
         {
-            client.Stop();
-        }
-    }
+            public SteamAccount Account { get; set; }
+            public Logger Logger { get; set; }
 
-    class Client {
-        public SteamAccount Account { get; set; }
-        public Logger Logger { get; set; }
+            public List<IService> Services { get; set; }
 
-        public List<IService> Services { get; set;}
-
-        public Client(Credentials cred) {
-            Account = new SteamAccount(cred);
-            Logger = new Logger(Account);
-            Services = new List<IService>() {
+            public Client(Credentials cred)
+            {
+                Account = new SteamAccount(cred);
+                Logger = new Logger(Account);
+                Services = new List<IService>() {
                 new LoginService(Account, Logger),
                 new ItemService(Account, Logger),
                 new PlayService(Account, Logger),
                 new EconService(Account, Logger),
                 new EconomyService(Account, Logger)
             };
-        }
-
-        public void Start()
-        {
-            foreach (var service in Services)
-            {
-                service.Start();
             }
-            Account.Connect();
-        }
 
-        public void Stop()
-        {
-            foreach(var service in Services)
+            public void Start()
             {
-                service.Stop();
+                foreach (var service in Services)
+                {
+                    service.Start();
+                }
+                Account.Connect();
             }
-            Account.Disconnect();
+
+            public void Stop()
+            {
+                foreach (var service in Services)
+                {
+                    service.Stop();
+                }
+                Account.Disconnect();
+            }
         }
     }
+
 }
